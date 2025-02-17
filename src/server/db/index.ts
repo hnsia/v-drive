9467fa99050528e8bba1142ea2196c91262ae8d0
hnsia <44498178+hnsia@users.noreply.github.com>
2025-02-18 01:49:31 +0800

    feat: setting up drizzle db

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,6 @@
 import { createClient, type Client } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import { createPool, type Pool } from "mysql2/promise";
 
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -10,7 +11,21 @@ import * as schema from "./schema";
  */
 const globalForDb = globalThis as unknown as {
   client: Client | undefined;
+  conn: Pool | undefined;
 };
+
+const conn =
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const client =
   globalForDb.client ?? createClient({ url: env.DATABASE_URL });
